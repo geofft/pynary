26633c88ebb75566fdfc07948f88df5f97b582a4
Geoffrey Thomas <geofft@ldpreload.com>
2024-09-16 21:58:50 -0400

    Flesh out docstrings and remove unused __setitem__ override

--- src/pynary/__init__.py
+++ src/pynary/__init__.py
@@ -5,14 +5,35 @@ import construct
 
 
 class _Body(dict):
-    """A mapping in which the body of a Pynary class is evaluted."""
+    """
+    A mapping in which the body of a Pynary class is evaluated.
+
+    Compared to a normal class being evaluated with locals() set to a
+    normal dict, fields that received a type annotation but no actual
+    assignment can be referenced, and they're passed to construct's
+    "this" feature. Consider the example in Construct's docs:
+
+        from construct import Struct, Int8ub, Array, this
+
+        image = Struct(
+            "width" / Int8ub,
+            "height" / Int8ub,
+            "pixels" / Array(this.width * this.height, Byte),
+        )
+
+    We would like to write it as:
+
+        class Image(Base):
+            width: Int8ub
+            height: Int8ub
+            pixels: Array(width * height, Byte)
+
+    but we need "width" and "height" to be usable in the annotation.
+    """
 
     def __init__(self):
         super().__init__()
         self["__annotations__"] = {}
-
-    def __setitem__(self, key, value):
-        super().__setitem__(key, value)
 
     def __getitem__(self, key):
         try:
@@ -24,6 +45,12 @@ class _Body(dict):
 
 
 class Meta(type):
+    """
+    The metaclass of Pynary classes.
+
+    You probably want to use Base instead of using this directly.
+    """
+
     @classmethod
     def __prepare__(cls, name, bases, **kwds):
         return _Body()
@@ -37,12 +64,26 @@ class Meta(type):
 
 
 class Base(metaclass=Meta):
+    """
+    The base class of Pynary classes.
+
+    This implicitly makes the class a dataclass.
+    """
+
     @classmethod
-    def parse(cls, stream) -> Self:
+    def parse(cls, stream: io.RawIOBase | io.BufferedIOBase) -> Self:
+        """Construct a new object by reading from a stream."""
         parsed = cls._struct.parse_stream(stream)
         return cls(**{k: v for k, v in parsed.items() if not k[0] == "_"})
 
     def __buffer__(self, flags: int) -> memoryview:
+        """
+        Serialize the object as a read-only memoryview.
+
+        This method implements the buffer protocol, allowing writing to
+        a stream with by calling the .write() method on an object
+        directly.
+        """
         return memoryview(self._struct.build(asdict(self)))
 
 
